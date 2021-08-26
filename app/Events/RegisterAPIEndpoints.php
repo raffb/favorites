02d361ddd401e00757b375d6801fa6c50f06b3e2
Raffaele Barone <rbaronework@gmail.com>
2021-08-26 13:12:45 +0200

    feat: add user favorite list endpoint

--- app/Events/RegisterAPIEndpoints.php
+++ app/Events/RegisterAPIEndpoints.php
@@ -1,32 +1,30 @@
 <?php
 namespace Favorites\Events;
 
-use Favorites\Listeners\REST\FavoriteCount;
+use Favorites\Listeners\REST\UserFavoriteList;
 use Favorites\Listeners\REST\FavoriteAdd;
 
 class RegisterAPIEndpoints
 {
 	public function __construct()
 	{
-		$namespaces = 'favorites/v1/posts';
+		$namespaces = 'favorites/v1';
 
-		register_rest_route( $namespaces, '/(?P<postid>\d+)', array(
+		register_rest_route( $namespaces, '/posts', array(
 			'methods' => 'GET',
-			'callback' => array($this, 'getFavoriteCount'),
+			'callback' => array($this, 'getUserFavoriteList'),
 			'args' => array(
-				'postid' => array(
-					'required' => true,
-					'description' => 'Specify a post to show. Defaults to the current post',
-					'type' => 'number'
-				),
 				'siteid' => array(
 					'description' => 'Specify a site ID if used in a multisite installation. Defaults to the current site/blog',
 					'type' => 'number'
 				)
-			)
+			),
+			'permission_callback' => function () {
+				return is_user_logged_in();
+			}
 		));
 
-		register_rest_route( $namespaces, '/(?P<postid>\d+)', array(
+		register_rest_route( $namespaces, '/posts/(?P<postid>\d+)', array(
 			'methods' => 'POST',
 			'callback' => array($this, 'addFavorite'),
 			'args' => array(
@@ -46,9 +44,9 @@ class RegisterAPIEndpoints
 		));
 	}
 
-	public function getFavoriteCount($WP_REST_Request)
+	public function getUserFavoriteList($WP_REST_Request)
 	{
-		new FavoriteCount($WP_REST_Request);
+		new UserFavoriteList($WP_REST_Request);
 	}
 
 	public function addFavorite($WP_REST_Request)
---
+++ app/Listeners/REST/UserFavoriteList.php
@@ -0,0 +1,40 @@
+<?php
+namespace Favorites\Listeners\REST;
+
+use Favorites\Entities\User\UserFavorites;
+
+/**
+* Return the total number of favorites for a specified post
+*/
+class UserFavoriteList extends ListenerBase
+{
+	/**
+	* Favorite Counter
+	*/
+	private $user_favorites;
+
+	public function __construct($WP_REST_Request)
+	{
+		parent::__construct();
+		$this->user_favorites = new UserFavorites;
+		$this->setData($WP_REST_Request);
+		$this->sendList();
+	}
+
+	private function setData($WP_REST_Request)
+	{
+		$siteId = $WP_REST_Request->get_param('siteid');
+		$userId = get_current_user_id();
+
+		$this->data['siteid'] = ( $siteId ) ? intval( $siteId ) : 1;
+		$this->data['user_id'] = ( $userId && $userId !== '' ) ? $userId : null;
+	}
+
+	private function sendList()
+	{
+		$this->response(array(
+			'status' => 'success',
+			'data' => $this->user_favorites->getFavoritesArray($this->data['user_id'], $this->data['siteid'])
+		));
+	}
+}
